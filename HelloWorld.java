66550ac847cd94b3965784354f2d80c475bffa41
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-05-30 12:17:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-class HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Hello, World!"); 
+//Write a program to print Hello World.
+
+class HelloWorld {                                  // Define a class named HelloWorld
+    public static void main(String args[]) {        // Main method, the entry point of the program
+        System.out.println("Hello, World!");        // Print the message "Hello, World!" to the console
     }
 }

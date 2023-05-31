9c12e7b2c6b8dd6cf77cab9a696089257ec87b4b
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-05-31 20:10:18 +0530

    Add files via upload

---
+++ Mathematical_Calculations.java
@@ -0,0 +1,12 @@
+public class Mathematical_Calculations
+{
+public static void main(String[] args)
+{
+double A = 10; 
+double B = 4; 
+System.out.println(A+B); 
+System.out.println(A-B); 
+System.out.println(A*B); 
+System.out.println(A/B); 
+}
+}

6350bf61244a9213313daca10a715261353ed766
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-05-31 20:14:04 +0530

    Update Mathematical_Calculations.java

--- Mathematical_Calculations.java
+++ Mathematical_Calculations.java
@@ -1,12 +1,11 @@
-public class Mathematical_Calculations
-{
-public static void main(String[] args)
-{
-double A = 10; 
-double B = 4; 
-System.out.println(A+B); 
-System.out.println(A-B); 
-System.out.println(A*B); 
-System.out.println(A/B); 
-}
+public class Mathematical_Calculations {
+    public static void main(String[] args) {
+        double A = 10;                          // Assigning the value 10 to the variable A
+        double B = 4;                           // Assigning the value 4 to the variable B
+        
+        System.out.println(A + B);              // Printing the sum of A and B
+        System.out.println(A - B);              // Printing the difference of A and B
+        System.out.println(A * B);              // Printing the product of A and B
+        System.out.println(A / B);              // Printing the quotient of A and B
+    }
 }

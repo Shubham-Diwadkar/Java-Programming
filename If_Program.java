956681233715e3587ed36fa4566b1c35e224f800
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-03 18:53:15 +0530

    Update If_Program.java

--- If_Program.java
+++ If_Program.java
@@ -4,13 +4,14 @@ class If_Program
 {
     public static void main(String[] args)
     {
-        int x = 10;
-
+        int x = 10;                             // Declaring and initializing the variable x with the value 10.
+        
+        // Checking if the value of x is equal to 10.
         if(x == 10)
         {
-            int y = 20;
-            System.out.println("x = "+x);
-            System.out.println("y = "+y);
+            int y = 20;                         // Declaring and initializing the variable y with the value 20.
+            System.out.println("x = "+x);       // Printing the value of x.
+            System.out.println("y = "+y);       // Printing the value of y.
         }
     }
 }

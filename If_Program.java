a87752be57cc1d7077779c29a1fda8a1a071f462
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-03 18:50:31 +0530

    Add files via upload

---
+++ If_Program.java
@@ -0,0 +1,16 @@
+// Write a program to demonstrate if program.
+
+class If_Program
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+
+        if(x == 10)
+        {
+            int y = 20;
+            System.out.println("x = "+x);
+            System.out.println("y = "+y);
+        }
+    }
+}

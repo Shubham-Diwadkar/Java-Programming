fef154e117dfa04a5c3b8530d77a33ef289b14ee
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-04 12:21:07 +0530

    Add files via upload

---
+++ If_Else_Program.java
@@ -0,0 +1,18 @@
+//Write a if-else program to check the time and wish the user accordingly.
+
+class If_Else_Program 
+{
+    public static void main(String[] args) 
+    {
+        int time = 20;
+
+        if(time < 18)
+        {
+            System.out.println("Good Day.");
+        }
+        else
+        {
+            System.out.println("Good Evening.");
+        }
+    }    
+}

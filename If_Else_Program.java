28adcd5f4c95d5680712454eaca2cee54d92e600
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-04 12:23:56 +0530

    Update If_Else_Program.java

--- If_Else_Program.java
+++ If_Else_Program.java
@@ -2,17 +2,17 @@
 
 class If_Else_Program 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        int time = 20;
+        int time = 20;                              // Declaring and initializing the variable time with the value 20.
 
-        if(time < 18)
+        if(time < 18)                               // Checking if the value of time is less than 18.
         {
-            System.out.println("Good Day.");
+            System.out.println("Good Day.");        // Printing "Good Day" if the condition is true.
         }
         else
         {
-            System.out.println("Good Evening.");
+            System.out.println("Good Evening.");    // Printing "Good Evening" if the condition is false.
         }
-    }    
+    }
 }

6e199b7d3a6b00a5dd9dd55aaec0088bce37c94e
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-04 13:21:53 +0530

    Update Else_If_Program.java

--- Else_If_Program.java
+++ Else_If_Program.java
@@ -4,18 +4,18 @@ public class Else_If_Program
 {
     public static void main(String[] args)
     {
-        int time = 22;
-        if (time < 10)
+        int time = 22;                                  // Declaring and initializing the variable time with the value 22.
+        if (time < 10)                                  // Checking if the value of time is less than 10.
         {
-            System.out.println("Good morning.");
+            System.out.println("Good morning.");        // Printing "Good morning" if the condition is true.
         }
-        else if (time < 18)
+        else if (time < 18)                             // Checking if the value of time is less than 18.
         {
-            System.out.println("Good day.");
+            System.out.println("Good day.");            // Printing "Good day" if the condition is true.
         }
         else
         {
-            System.out.println("Good evening.");
+            System.out.println("Good evening.");        // Printing "Good evening" if all conditions are false.
         }
     }
 }

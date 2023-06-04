6a4249d678126ea6709f1cc8023d01a1923a1d4a
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-04 13:05:46 +0530

    Update Mathematical_Calculations.java

--- Mathematical_Calculations.java
+++ Mathematical_Calculations.java
@@ -1,3 +1,5 @@
+//Write a program to do Mathematical Calulations in Java
+
 public class Mathematical_Calculations {
     public static void main(String[] args) {
         double A = 10;                          // Assigning the value 10 to the variable A
@@ -7,5 +9,9 @@ public class Mathematical_Calculations {
         System.out.println(A - B);              // Printing the difference of A and B
         System.out.println(A * B);              // Printing the product of A and B
         System.out.println(A / B);              // Printing the quotient of A and B
+        System.out.println(A % B);              // Printing the Modulo (Remainder) of A and B
+        System.out.println(Math.pow(A, B));     // Printing the Exponentiation (Power) of A and B
+        System.out.println(Math.sqrt(A));       // Printing the Square Root of A
+        System.out.println(Math.sqrt(B));       // Printing the Square Root of B
     }
 }

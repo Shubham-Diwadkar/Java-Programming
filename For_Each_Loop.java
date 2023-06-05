da4aeef847922f5a5fa5579a0546ee9409b30a50
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 22:12:13 +0530

    Add files via upload

---
+++ For_Each_Loop.java
@@ -0,0 +1,13 @@
+// Write a program to illustrate for-each loop in java.
+
+public class For_Each_Loop
+{
+    public static void main(String[] args)
+    {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars)
+        {
+            System.out.println(i);
+        }
+    }
+}

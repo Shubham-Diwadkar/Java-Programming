1d3b6b4e5a16e111b5cf0a9370c50c9ef77add80
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 20:55:07 +0530

    Add files via upload

---
+++ While_Loop.java
@@ -0,0 +1,12 @@
+class While_Loop
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i<6)
+        {
+            System.out.println("Value of i: " + i);
+            i++;
+        }    
+    }
+}

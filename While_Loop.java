b40733461a38544ede710a9b9989fb4e7cad0187
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 20:44:54 +0530

    Add files via upload

---
+++ While_Loop.java
@@ -0,0 +1,13 @@
+// Write a program to illustrate while loop in Java.
+
+class While_Loop {
+	public static void main(String args[])
+	{
+		int i = 1;                                      // initialization expression
+		while (i < 6)                                   // test expression
+        {
+			System.out.println("Value of i: " + i);     // printing value of i
+			i++;                                        // update expression
+		}
+	}
+}

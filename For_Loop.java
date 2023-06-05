d092879914234388130dad2207343cc78f121835
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 21:46:49 +0530

    Add files via upload

---
+++ For_Loop.java
@@ -0,0 +1,13 @@
+// Write a program to illustrate for loop in java
+
+public class For_Loop
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i = 0; i < 5; i++)
+        {
+            System.out.println("Value of i: " + i);
+        }
+    }
+}

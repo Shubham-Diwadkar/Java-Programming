ec5ffb7a10fec5db0b2e2246919ba83fbb4671e8
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 22:16:11 +0530

    Update For_Each_Loop.java

--- For_Each_Loop.java
+++ For_Each_Loop.java
@@ -4,10 +4,10 @@ public class For_Each_Loop
 {
     public static void main(String[] args)
     {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};          // Declare and initialize a String array.
         for (String i : cars)
         {
-            System.out.println(i);
+            System.out.println("Car Name:"+i);                      // Print each element in the array.
         }
     }
 }

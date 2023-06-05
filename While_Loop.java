cd8c8c13db1f8e24f19a133068603e54e007d4f4
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 20:58:50 +0530

    Update While_Loop.java

--- While_Loop.java
+++ While_Loop.java
@@ -1,12 +1,14 @@
+// Write a program to illustrate while loop in Java.
+
 class While_Loop
 {
     public static void main(String[] args)
     {
-        int i = 1;
-        while(i<6)
+        int i = 1;                                      // Declaring and initializing the variable i with the value 1.
+        while(i<6)                                      // Checking if the value of i is less than 6.
         {
-            System.out.println("Value of i: " + i);
-            i++;
+            System.out.println("Value of i: " + i);     // Printing the value of i.
+            i++;                                        // Incrementing the value of i by 1.
         }    
     }
 }

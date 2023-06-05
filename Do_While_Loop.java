ba2e6a970764162ffca0d760d3d49f320e38a188
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 21:43:45 +0530

    Update Do_While_Loop.java

--- Do_While_Loop.java
+++ Do_While_Loop.java
@@ -1,12 +1,14 @@
+// Write a program to illustrate do-while loop in java
+
 public class Do_While_Loop
 {
     public static void main(String[] args) 
     {
-        int i = 0;
+        int i = 0;                                      // Declare and initialize the variable i with the value 0.
         do
         {
-            System.out.println("Value of i: "+i);
-            i++;
-        } while(i < 5);
+            System.out.println("Value of i: "+i);       // Print the value of i.
+            i++;                                        // Increment the value of i by 1.
+        } while(i < 5);                                 // Continue the loop as long as i is less than 5.
     }    
 }

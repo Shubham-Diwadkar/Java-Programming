b3a42ceab4ee749960a462e3d19e055cec9aa928
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-05 21:28:55 +0530

    Add files via upload

---
+++ Do_While_Loop.java
@@ -0,0 +1,12 @@
+public class Do_While_Loop
+{
+    public static void main(String[] args) 
+    {
+        int i = 0;
+        do
+        {
+            System.out.println("Value of i: "+i);
+            i++;
+        } while(i < 5);
+    }    
+}

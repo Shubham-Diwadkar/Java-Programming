6c35c2fe475192787edcfb2cc3011ca276bd5a11
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-06 22:39:18 +0530

    Update Multi_Dimentional_Arrays.java

--- Multi_Dimentional_Arrays.java
+++ Multi_Dimentional_Arrays.java
@@ -2,8 +2,8 @@ public class Multi_Dimentional_Arrays
 {
     public static void main(String[] args)
     {
-        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
-        for (int i = 0; i < myNumbers.length; ++i)
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };                                                    // Declare and initialize a 2D array
+        for (int i = 0; i < myNumbers.length; ++i)                                                          // Print the elements of the 2D array using nested loops
         {
             for(int j = 0; j < myNumbers[i].length; ++j)
             {
@@ -12,11 +12,11 @@ public class Multi_Dimentional_Arrays
         }
         System.out.print("\n");
 
-        System.out.println("Accessing 3rd element of 2 array: "+myNumbers[1][2]);
+        System.out.println("Accessing 3rd element of 2 array: "+myNumbers[1][2]);                           // Accessing a specific element in the 2D array
         System.out.print("\n");
         
         System.out.println("CHANGING THE ACCESSED ELEMENT");
-        myNumbers[1][2] = 9;
+        myNumbers[1][2] = 9;                                                                                // Change the value of an accessed element
         
         System.out.println("AFTER CHANGING THE ACCESSED ELEMENT");
         System.out.print("\n");
@@ -25,17 +25,17 @@ public class Multi_Dimentional_Arrays
         {
             for(int j = 0; j < myNumbers[i].length; ++j)
             {
-              System.out.println(myNumbers[i][j]);
+              System.out.println(myNumbers[i][j]);                                                          // Print the elements of the modified 2D array
             }
         }
         System.out.print("\n");
 
-        String[][] myColors = { {"Red", "Yellow", "Green", "Blue"}, {"Purple", "Black", "White"} };
+        String[][] myColors = { {"Red", "Yellow", "Green", "Blue"}, {"Purple", "Black", "White"} };         // Declare and initialize a 2D array of strings
         for (int i = 0; i < myColors.length; ++i)
         {
             for(int j = 0; j < myColors[i].length; ++j)
             {
-                System.out.println(myColors[i][j]);
+                System.out.println(myColors[i][j]);                                                          // Print the elements of the 2D array of strings using nested loops
             }
         }
     }

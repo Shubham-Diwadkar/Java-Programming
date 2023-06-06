7b54a994d6d8d11920980dd64455725f34bc4c74
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-06 21:40:11 +0530

    Update Single_Dimensional_Arrays.java

--- Single_Dimensional_Arrays.java
+++ Single_Dimensional_Arrays.java
@@ -4,30 +4,30 @@ public class Single_Dimensional_Arrays
     {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
         System.out.println("Cars in Array: ");
-        System.out.println(cars[0]);
-        System.out.println(cars[1]);
-        System.out.println(cars[2]);
-        System.out.println(cars[3]);
+        System.out.println(cars[0]);                                // Print the first element of the cars array
+        System.out.println(cars[1]);                                // Print the second element of the cars array
+        System.out.println(cars[2]);                                // Print the third element of the cars array
+        System.out.println(cars[3]);                                // Print the fourth element of the cars array
         System.out.print("\n");
         
-        cars[0] = "Opel";
+        cars[0] = "Opel";                                           // Change the value of the first element
         System.out.println("AFTER CHANGING THE ELEMENT:-");
         System.out.print("\n");
         System.out.println("Cars in Array: ");
-        System.out.println(cars[0]);
-        System.out.println(cars[1]);
-        System.out.println(cars[2]);
-        System.out.println(cars[3]);
+        System.out.println(cars[0]);                                // Print the first element of the cars array after the change
+        System.out.println(cars[1]);                                // Print the second element of the cars array after the change
+        System.out.println(cars[2]);                                // Print the third element of the cars array after the change
+        System.out.println(cars[3]);                                // Print the fourth element of the cars array after the change
         System.out.print("\n");
 
-        System.out.println("Length of the Array: "+cars.length);
+        System.out.println("Length of the Array: "+cars.length);    // Print the length of the cars array
         System.out.print("\n");
 
         String[] fruits = {"Mango", "Banana", "Apple", "Grapes"};
         System.out.println("Fruits in Array: ");
         for (int i = 0; i < fruits.length; i++)
         {
-            System.out.println(fruits[i]);
+            System.out.println(fruits[i]);                            // Print each element of the fruits array using an index-based approach
         }
         System.out.print("\n");
 
@@ -35,7 +35,7 @@ public class Single_Dimensional_Arrays
         System.out.println("Numbers in Array: ");
         for (int j : numbers)
         {
-            System.out.println(j);
+            System.out.println(j);                                    // Print each element of the numbers array using a for-each loop
         }
     }
 }

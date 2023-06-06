cc034d015438340356c4ebd91d29b9d38a1c6949
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-06 21:35:18 +0530

    Add files via upload

---
+++ Single_Dimensional_Arrays.java
@@ -0,0 +1,41 @@
+public class Single_Dimensional_Arrays
+{
+    public static void main(String[] args)
+    {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println("Cars in Array: ");
+        System.out.println(cars[0]);
+        System.out.println(cars[1]);
+        System.out.println(cars[2]);
+        System.out.println(cars[3]);
+        System.out.print("\n");
+        
+        cars[0] = "Opel";
+        System.out.println("AFTER CHANGING THE ELEMENT:-");
+        System.out.print("\n");
+        System.out.println("Cars in Array: ");
+        System.out.println(cars[0]);
+        System.out.println(cars[1]);
+        System.out.println(cars[2]);
+        System.out.println(cars[3]);
+        System.out.print("\n");
+
+        System.out.println("Length of the Array: "+cars.length);
+        System.out.print("\n");
+
+        String[] fruits = {"Mango", "Banana", "Apple", "Grapes"};
+        System.out.println("Fruits in Array: ");
+        for (int i = 0; i < fruits.length; i++)
+        {
+            System.out.println(fruits[i]);
+        }
+        System.out.print("\n");
+
+        int[] numbers = {10, 20, 30, 40};
+        System.out.println("Numbers in Array: ");
+        for (int j : numbers)
+        {
+            System.out.println(j);
+        }
+    }
+}

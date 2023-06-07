60f85bbd5317204b93f305afecd05ac173717a3a
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-07 12:59:12 +0530

    Update Multiple_Classes1.java

--- Multiple_Classes1.java
+++ Multiple_Classes1.java
@@ -1,4 +1,8 @@
+// Write a program to demonstrate the use of Multiple Classes in java
+
 public class Multiple_Classes1
 {
     int x = 5;
 }
+
+// To go further click the Multiple_Classes2.java File

9bec89e26d9327c4eacaa52c4f681098e8219957
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-07 12:31:35 +0530

    Add files via upload

---
+++ Basic_Classes_Objects.java
@@ -0,0 +1,9 @@
+public class Basic_Classes_Objects
+{
+    int x = 5;
+    public static void main(String[] args)
+    {
+        Basic_Classes_Objects myObj = new Basic_Classes_Objects();
+        System.out.println("Variable in Outside main(): "+myObj.x);        
+    }
+}

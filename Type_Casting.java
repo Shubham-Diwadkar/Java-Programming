ceca5b5b339a95bdb984dfd0a4806a24e7272668
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-07 11:06:27 +0530

    Update Type_Casting.java

--- Type_Casting.java
+++ Type_Casting.java
@@ -3,12 +3,12 @@ public class Type_Casting
     public static void main(String[] args)
     {
         int myInt1 = 9;
-        double myDouble1 = myInt1;
+        double myDouble1 = myInt1;                              // Implicit type casting from int to double
         System.out.println("Integer Value: "+myInt1);
         System.out.println("Double Value: "+myDouble1);
 
         double myDouble2 = 9.78d;
-        int myInt2 = (int) myDouble2;
+        int myInt2 = (int) myDouble2;                           // Explicit type casting from double to int
         System.out.println("Double Value: "+myDouble2);
         System.out.println("Integer Value: "+myInt2);
     }

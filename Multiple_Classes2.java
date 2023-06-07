dc06681b8eac2f1c6f1e860c0642359d4620e715
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-07 13:00:14 +0530

    Update Multiple_Classes2.java

--- Multiple_Classes2.java
+++ Multiple_Classes2.java
@@ -1,3 +1,5 @@
+// Write a program to demonstrate the use of Multiple Classes in java
+
 public class Multiple_Classes2
 {
     public static void main(String[] args)
@@ -6,3 +8,5 @@ public class Multiple_Classes2
         System.out.println("Accessing Variable of Multiple_Classes1 in Multiple_Classes2: "+MC1.x);   
     }    
 }
+
+// The variable is been declared in Multiple_Classes1.java File

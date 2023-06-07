444145694687beadaae72368db38c08392ee412b
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-07 12:37:46 +0530

    Update Basic_Classes_Objects.java

--- Basic_Classes_Objects.java
+++ Basic_Classes_Objects.java
@@ -3,7 +3,10 @@ public class Basic_Classes_Objects
     int x = 5;
     public static void main(String[] args)
     {
-        Basic_Classes_Objects myObj = new Basic_Classes_Objects();
-        System.out.println("Variable in Outside main(): "+myObj.x);        
+        Basic_Classes_Objects myObj1 = new Basic_Classes_Objects();
+        Basic_Classes_Objects myObj2 = new Basic_Classes_Objects();
+        System.out.println("Accessing Variable Outside main() using First Object: "+myObj1.x);
+        System.out.print("\n");
+        System.out.println("Accessing Variable Outside main() using Second Object: "+myObj2.x);        
     }
 }

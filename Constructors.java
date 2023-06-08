61f3512c6a3b9d9ee3f32954a999d0217b6db74d
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-08 20:00:54 +0530

    Update Constructors.java

--- Constructors.java
+++ Constructors.java
@@ -1,15 +1,15 @@
 // Write a program to demonstrate the use of default contructor.
 
-public class Constructors
+public class Defualt_Constructors
 {
-    int x;                                                                                  // Instance variable x
-    public Constructors()
+    int x;                                                                                      // Instance variable x
+    public Default_Constructors()
     {
-        x = 5;                                                                              // Constructor to initialize the value of x to 5
+        x = 5;                                                                                  // Constructor to initialize the value of x to 5
     }
     public static void main(String[] args)
     {
-        Constructors myConstructors = new Constructors();                                   // Creating an object of the Constructors class
-        System.out.println("Value fetched from the Constructor: "+myConstructors.x);
+        Default_Constructors myDefault_Constructors = new Default_Constructors();               // Creating an object of the Constructors class
+        System.out.println("Value fetched from the Constructor: "+myDefault_Constructors.x);
     }
 }

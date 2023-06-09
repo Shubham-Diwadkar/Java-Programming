0cd6de677c2cca5e117035cbc8d0a9b1f72ab7e0
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-09 11:49:23 +0530

    Update Parameterized_Constructors.java

--- Parameterized_Constructors.java
+++ Parameterized_Constructors.java
@@ -1,17 +1,19 @@
+// Write a program to demonstrate Parameterized Constructor in java.
+
 public class Parameterized_Constructors
 {
-    int modelYear;
-    String modelName;
+    int modelYear;                                                                                              // Instance variable to store the model year
+    String modelName;                                                                                           // Instance variable to store the model name
 
     public Parameterized_Constructors(int year, String name)
     {
-        modelYear = year;
-        modelName = name;
+        modelYear = year;                                                                                       // Initialize the model year with the provided parameter value
+        modelName = name;                                                                                       // Initialize the model name with the provided parameter value
     }
 
     public static void main(String[] args)
     {
-        Parameterized_Constructors myCar = new Parameterized_Constructors(1969, "Mustang");
-        System.out.println("Car Year: " + myCar.modelYear + "\n \n Car Name: " + myCar.modelName);
+        Parameterized_Constructors myCar = new Parameterized_Constructors(1969, "Mustang");                     // Create an object using the parameterized constructor
+        System.out.println("Car Year: " + myCar.modelYear + "\n \n Car Name: " + myCar.modelName);              // Print the values of modelYear and modelName
     }
 }

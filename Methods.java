f45ec9a8362254b22caf40f0630fa5c1d0d114f3
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-10 12:34:34 +0530

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -4,12 +4,12 @@ public class Methods
 {
     static void myMethod()
     {
-        System.out.println("\nI am inside a Method Call!");
+        System.out.println("\nI am inside a Method Call!");                  // Prints "I am inside a Method Call!"
     }
     public static void main(String[] args)
     {
-        System.out.println("I am outside a Method Call!");
-        myMethod();
-        System.out.println("\nI am outside a Method Call again!");
+        System.out.println("I am outside a Method Call!");                   // Prints "I am outside a Method Call!"
+        myMethod();                                                          // Calls the method myMethod()
+        System.out.println("\nI am outside a Method Call again!");           // Prints "I am outside a Method Call again!"
     }
 }

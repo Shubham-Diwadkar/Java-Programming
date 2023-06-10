f7d59d40789dd35d430538f27beb912799b1c5c6
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-10 13:35:06 +0530

    Update Parameterized_Methods.java

--- Parameterized_Methods.java
+++ Parameterized_Methods.java
@@ -2,12 +2,12 @@ public class Parameterized_Methods
 {
     static void myMethod(String fname)
     {
-        System.out.println("My name is "+fname);
+        System.out.println("My name is "+fname);            // Prints the names passed by the method
     }
     public static void main(String[] args)
     {
-        myMethod("Liam");
-        myMethod("Shubham");
-        myMethod("Anja");
+        myMethod("Liam");                                   // Calls myMethod() with argument "Liam"
+        myMethod("Shubham");                                // Calls myMethod() with argument "Shubham"
+        myMethod("Anja");                                   // Calls myMethod() with argument "Anja"
     }
 }

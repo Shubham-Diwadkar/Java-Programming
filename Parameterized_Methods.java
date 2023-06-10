fd74fbd493bf8b8bc9a7fd1935a127990004135b
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-10 13:27:26 +0530

    Add files via upload

---
+++ Parameterized_Methods.java
@@ -0,0 +1,13 @@
+public class Parameterized_Methods
+{
+    static void myMethod(String fname)
+    {
+        System.out.println("My name is "+fname);
+    }
+    public static void main(String[] args)
+    {
+        myMethod("Liam");
+        myMethod("Shubham");
+        myMethod("Anja");
+    }
+}

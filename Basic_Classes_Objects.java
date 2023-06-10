67f6cefc410ab927c44d6a129ff39d8f1683e5ce
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-10 11:42:06 +0530

    Update Basic_Classes_Objects.java

--- Basic_Classes_Objects.java
+++ Basic_Classes_Objects.java
@@ -10,5 +10,9 @@ public class Basic_Classes_Objects
         System.out.println("Accessing Variable Outside main() using First Object: "+myObj1.x);  // Printing the variable using first object
         System.out.print("\n");
         System.out.println("Accessing Variable Outside main() using Second Object: "+myObj2.x); // Printing the variable using first object
+        System.out.println("\nIs myObj1 instance of Basic Classes Object:");
+        System.out.println(myObj1 instanceof Basic_Classes_Objects);                            // Checks if myObj1 is an instance of Basic_Classes_Objects and prints the result
+        System.out.println("\nIs myObj2 instance of Basic Classes Object:");
+        System.out.println(myObj2 instanceof Basic_Classes_Objects);                            // Checks if myObj2 is an instance of Basic_Classes_Objects and prints the result
     }
 }

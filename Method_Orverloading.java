8bf8b43611e02637e190ada791b275ca7786ce51
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-11 12:59:43 +0530

    Update Method_Orverloading.java

--- Method_Orverloading.java
+++ Method_Orverloading.java
@@ -1,18 +1,20 @@
+// Write a program to demonstrate Method Overloading
+
 public class Method_Orverloading
 {
-    static int plusMethod(int x, int y)
+    static int plusMethod(int x, int y)                     // Method that takes two integers and returns their sum
     {
         return(x + y);
     }
-    static double plusMethod(double x, double y)
+    static double plusMethod(double x, double y)            // Method that takes two doubles and returns their sum
     {
         return(x + y);
     }
     public static void main(String[] args)
     {
-        int myNum1 = plusMethod(8, 5);
-        double myNum2 = plusMethod(4.3, 6.26);
-        System.out.println("int: "+myNum1);
-        System.out.println("double: "+myNum2);
+        int myNum1 = plusMethod(8, 5);                      // Calling plusMethod with integers
+        double myNum2 = plusMethod(4.3, 6.26);              // Calling plusMethod with doubles
+        System.out.println("int: "+myNum1);                 // Printing the result of the integer addition
+        System.out.println("double: "+myNum2);              // Printing the result of the double addition
     }
 }

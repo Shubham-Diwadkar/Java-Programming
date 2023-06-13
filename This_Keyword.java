e53a1d5e14c4b8e3ec7fafa7b1e4c7933d61a662
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-13 13:53:49 +0530

    Update This_Keyword.java

--- This_Keyword.java
+++ This_Keyword.java
@@ -1,13 +1,15 @@
+// Write a program to demonstrate the use of this keyword in java
+
 public class This_Keyword
 {
     private int x;
     public This_Keyword(int x)
     {
-        this.x = x;
+        this.x = x;                                                 // Assigns the value of the parameter x to the instance variable x using the "this" keyword
     }
     public static void main(String[] args)
     {
-        This_Keyword myObjThisKeyword = new This_Keyword(5);
-        System.out.println("Value of x = " + myObjThisKeyword.x);
+        This_Keyword myObjThisKeyword = new This_Keyword(5);        // Creates an instance of the This_Keyword class with x = 5
+        System.out.println("Value of x = " + myObjThisKeyword.x);   // Prints the value of the instance variable x
     }
 }

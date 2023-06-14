bb87c83170dce17e9979671f94beff330ad561ca
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-14 22:35:14 +0530

    Update Finalize_Method.java

--- Finalize_Method.java
+++ Finalize_Method.java
@@ -2,17 +2,17 @@ public class Finalize_Method
 {
     public static void main(String[] args)
     {
-        Finalize_Method objFinalize_Method = new Finalize_Method();
-        System.out.println(objFinalize_Method.hashCode());
-        objFinalize_Method = null;
+        Finalize_Method objFinalize_Method = new Finalize_Method();     // Creates an object of the Finalize_Method class
+        System.out.println(objFinalize_Method.hashCode());              // Prints the hash code of the object
+        objFinalize_Method = null;                                      // Sets the reference variable to null, making the object eligible for garbage collection
 
-        System.gc();
-        System.out.println("End of Garbage Collector");
+        System.gc();                                                    // Explicitly requests the JVM to perform garbage collection
+        System.out.println("End of Garbage Collector");                 // Prints a message after the garbage collection is completed
     }
 
     @Override
     protected void finalize()
     {
-        System.out.println("Finalize Method Called...");
+        System.out.println("Finalize Method Called...");                // Prints a message when the finalize method is called
     }
 }

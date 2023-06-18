1a00956ef8e1764b15cf3b054c99c0bcd6e6fecc
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-18 20:16:53 +0530

    Add files via upload

---
+++ Custom_Wrapper_Classes.java
@@ -0,0 +1,36 @@
+class CustomWrapper
+{
+    private int i;
+    CustomWrapper()
+    {
+
+    }
+    
+    CustomWrapper(int i)
+    {
+        this.i = i;
+    }
+    
+    public int getValue()
+    {
+        return i;
+    }
+
+    public void setValue(int i)
+    {
+        this.i = i;
+    }
+
+    public String toString()
+    {
+        return Integer.toString(i);
+    }
+}
+public class Custom_Wrapper_Classes
+{
+    public static void main(String[] args)
+    {
+        CustomWrapper cw = new CustomWrapper(10);
+        System.out.println("Value is: "+cw);
+    }
+}

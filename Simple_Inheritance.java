2fd23a5964157f3ae9edb2fcd5af750566b0c21e
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-19 22:45:46 +0530

    Update Simple_Inheritance.java

--- Simple_Inheritance.java
+++ Simple_Inheritance.java
@@ -1,10 +1,12 @@
+// Write a program to demonstrate Simple Inheritance in java
+
 class Employee
 {
-    float salary = 34534*12;
+    float salary = 34534*12;                           // Salary calculated as a product of monthly salary and 12 months
 }
 public class Simple_Inheritance extends Employee
 {
-    float bonus = 3000*6;
+    float bonus = 3000*6;                              // Bonus calculated as a product of monthly bonus and 6 months
     public static void main(String[] args)
     {
         Simple_Inheritance simple_InheritanceObj = new Simple_Inheritance();

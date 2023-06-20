af1dd29745e158c5f0c845252dcd1871373f2236
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-20 23:42:08 +0530

    Update Multiple_Inheritance.java

--- Multiple_Inheritance.java
+++ Multiple_Inheritance.java
@@ -2,14 +2,14 @@
 
 class Student
 {
-    int reg_no;// Variable to store registration number
+    int reg_no;            // Variable to store registration number
     
-    void getNo(int no)// Method to set the registration number
+    void getNo(int no)            // Method to set the registration number
     {
         reg_no = no;
     }
     
-    void putNo()// Method to display the registration number
+    void putNo()            // Method to display the registration number
     {
         System.out.println("Registration number = "+reg_no);
     }
@@ -17,14 +17,14 @@ class Student
 
 class Marks extends Student
 {
-    float marks;// Variable to store marks
+    float marks;            // Variable to store marks
     
-    void getMarks(float m)// Method to set the marks
+    void getMarks(float m)            // Method to set the marks
     {
         marks = m;
     }
     
-    void putMarks()// Method to display the marks
+    void putMarks()            // Method to display the marks
     {
         System.out.println("Marks = "+marks);
     }
@@ -32,14 +32,14 @@ class Marks extends Student
 
 class Sports extends Marks
 {
-    double score;// Variable to store score
+    double score;            // Variable to store score
     
-    void getScore(double scr)// Method to set the score
+    void getScore(double scr)            // Method to set the score
     {
         score = scr;
     }
     
-    void putScore()// Method to display the score
+    void putScore()            // Method to display the score
     {
         System.out.println("Score = "+score);
     }
@@ -49,16 +49,16 @@ public class Multiple_Inheritance
 {
     public static void main(String[] args)
     {
-        Sports sportsObj = new Sports();// Create an object of the Sports class
+        Sports sportsObj = new Sports();            // Create an object of the Sports class
         
-        sportsObj.getNo(987);// Set the registration number using the method from the Student class
-        sportsObj.putNo();// Display the registration number using the method from the Student class
+        sportsObj.getNo(987);            // Set the registration number using the method from the Student class
+        sportsObj.putNo();            // Display the registration number using the method from the Student class
         
-        sportsObj.getMarks(78);// Set the marks using the method from the Marks class
-        sportsObj.putMarks();// Display the marks using the method from the Marks class
+        sportsObj.getMarks(78);            // Set the marks using the method from the Marks class
+        sportsObj.putMarks();            // Display the marks using the method from the Marks class
         
-        sportsObj.getScore(68.7);// Set the score using the method from the Sports class
-        sportsObj.putScore();// Display the score using the method from the Sports class
+        sportsObj.getScore(68.7);            // Set the score using the method from the Sports class
+        sportsObj.putScore();            // Display the score using the method from the Sports class
 
     }
 }

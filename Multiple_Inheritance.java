b5fcbfa46e9d59e93c881699a9f07e6d88cdcdfc
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-06-24 23:45:52 +0530

    Update Multiple_Inheritance.java

--- Multiple_Inheritance.java
+++ Multiple_Inheritance.java
@@ -1,20 +1,22 @@
+// Write a program to implement the Multiple Inheritance in java using interfaces
+
 interface Wishes
 {
-    void message();
+    void message();            // Interface defining a message method
 }
 
 interface Birthday
 {
-    void message();
+    void message();            // Interface defining a message method
 }
 
 class Greetings implements Wishes, Birthday
 {
     public void message()
     {
-        System.out.println("\nBest of Luck!!!");
+        System.out.println("\nBest of Luck!!!");            // Implementation of the message method from Wishes interface
 
-        System.out.println("\nHappy Birthday!!!");
+        System.out.println("\nHappy Birthday!!!");            // Implementation of the message method from Birthday interface
     }
 }
 
@@ -22,8 +24,8 @@ public class Multiple_Inheritance
 {
     public static void main(String[] args)
     {
-        Greetings greetingsObj = new Greetings();
+        Greetings greetingsObj = new Greetings();            // Create an instance of the Greetings class
 
-        greetingsObj.message();
+        greetingsObj.message();            // Call the message method, which outputs the desired messages
     }
 }
